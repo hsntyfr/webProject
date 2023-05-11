a465878611f197e550876751eaa9d5f67f5d752c
Hasan Tayfur <hasantayfur69@hotmail.com>
2023-05-11 15:44:42 +0300

    api is fully okay

--- webProject/app.js
+++ webProject/app.js
@@ -1,5 +1,5 @@
 const url = 'http://ergast.com/api/f1/'
-const urlBack = '.json?callback=myParser'
+const  back = "/results.json"
 
 
 const setQuery = (event) => {
@@ -7,25 +7,47 @@ const setQuery = (event) => {
     {
         getResult(searchBar.value)
     }
+}
+async function getResult(year)
+{
+    let api = `${url}${year}${back}`
+    const result = await fetch(api);
+    const data = await result.json();
+    let info = data
+    console.log(info);
+    document.getElementById("name-1.1").innerHTML = info.MRData.RaceTable.Races[0].Results[0].Driver.familyName;
+    document.getElementById("car-1.1").innerHTML = info.MRData.RaceTable.Races[0].Results[0].Constructor.name;
+    document.getElementById("time-1.1").innerHTML = info.MRData.RaceTable.Races[0].Results[0].Time.time;
+    document.getElementById("name-1.2").innerHTML = info.MRData.RaceTable.Races[0].Results[1].Driver.familyName;
+    document.getElementById("car-1.2").innerHTML = info.MRData.RaceTable.Races[0].Results[1].Constructor.name;
+    document.getElementById("time-1.2").innerHTML = info.MRData.RaceTable.Races[0].Results[1].Time.time;
+    document.getElementById("name-1.3").innerHTML = info.MRData.RaceTable.Races[0].Results[2].Driver.familyName;
+    document.getElementById("car-1.3").innerHTML = info.MRData.RaceTable.Races[0].Results[2].Constructor.name;
+    document.getElementById("time-1.3").innerHTML = info.MRData.RaceTable.Races[0].Results[2].Time.time;
+    document.getElementById("name-1.4").innerHTML = info.MRData.RaceTable.Races[0].Results[3].Driver.familyName;
+    document.getElementById("car-1.4").innerHTML = info.MRData.RaceTable.Races[0].Results[3].Constructor.name;
+    document.getElementById("time-1.4").innerHTML = "---";
+    document.getElementById("name-1.5").innerHTML = info.MRData.RaceTable.Races[0].Results[4].Driver.familyName;
+    document.getElementById("car-1.5").innerHTML = info.MRData.RaceTable.Races[0].Results[4].Constructor.name;
+    document.getElementById("time-1.5").innerHTML = "---";
+    document.getElementById("name-2.1").innerHTML = info.MRData.RaceTable.Races[1].Results[0].Driver.familyName;
+    document.getElementById("car-2.1").innerHTML = info.MRData.RaceTable.Races[1].Results[0].Constructor.name;
+    document.getElementById("time-2.1").innerHTML = info.MRData.RaceTable.Races[1].Results[0].Time.time;
+    document.getElementById("name-2.2").innerHTML = info.MRData.RaceTable.Races[1].Results[1].Driver.familyName;
+    document.getElementById("car-2.2").innerHTML = info.MRData.RaceTable.Races[1].Results[1].Constructor.name;
+    document.getElementById("time-2.2").innerHTML = info.MRData.RaceTable.Races[1].Results[1].Time.time;
+    document.getElementById("name-2.3").innerHTML = info.MRData.RaceTable.Races[1].Results[2].Driver.familyName;
+    document.getElementById("car-2.3").innerHTML = info.MRData.RaceTable.Races[1].Results[2].Constructor.name;
+    document.getElementById("time-2.3").innerHTML = info.MRData.RaceTable.Races[1].Results[2].Time.time;
+    document.getElementById("name-2.4").innerHTML = info.MRData.RaceTable.Races[1].Results[3].Driver.familyName;
+    document.getElementById("car-2.4").innerHTML = info.MRData.RaceTable.Races[1].Results[3].Constructor.name;
+    document.getElementById("time-2.4").innerHTML = "---";
+    document.getElementById("name-2.5").innerHTML = info.MRData.RaceTable.Races[1].Results[4].Driver.familyName;
+    document.getElementById("car-2.5").innerHTML = info.MRData.RaceTable.Races[1].Results[4].Constructor.name;
+    document.getElementById("time-2.5").innerHTML = "---";
+
 
 }
-const getResult = (year) => {
-    let query = `${url}${year}${urlBack}`
-    console.log(query);
-    fetch(query)
-        .then(result => {
-        return result;
-    })
-        .then(displayResult)
-}
 
-const displayResult = (result) => {
-    let city = result;
-    console.log(city);
-};
-
-
-
-const searchBar = document.getElementById('serachBar')
+const searchBar = document.getElementById('searchBar')
 searchBar.addEventListener('keypress', setQuery)
-
